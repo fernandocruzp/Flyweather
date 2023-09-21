2a785412bfc3b87da52b0a1c57a1b3106400e95c
itzelfloresc <itzelflores@ciencias.unam.mx>
2023-09-20 21:42:19 -0600

    Para ignorar errores de escritura en el nombre de las ciudades(ajustar)

---
+++ website/matchingNames.py
@@ -0,0 +1,24 @@
+from thefuzz import process
+
+def matchea_ciudades(recibido):
+    return mejor_match(recibido).upper()
+
+ciudades_real = ['monterrey', 'mty', 'ciudad de mexico', 'mex', 'tlc', 'tijuana', 'tij',
+                 'hermosillo', 'hmo', 'ciudad del carmen', 'cme', 'chetumal', 'ctm',
+                 'veracruz', 'ver', 'oaxaca', 'oax', 'huatulco', 'hux', 'ixtapa zihuatanejo', 'zih', 'puerto vallarta','pvr',
+                 'lima', 'lim', 'havana', 'hav', 'bogota', 'bog', 'miami', 'mia', 'los ángeles', 'lax',
+                 'new york', 'jfk', 'tampico', 'tam', 'torreón', 'trc', 'puerto escondido', 'pxm',
+                 'acapulco', 'aca', 'mazatlan', 'mzt', 'guatemala', 'gua', 'aguascalientes', 'agu',
+                 'cancun', 'cun', 'mérida', 'mid', 'villahermosa', 'vsa', 'belize', 'bze', 'guadalajara', 'gdl',
+                 'dallas', 'dfw', 'cozumel', 'czm', 'chicago', 'ord', 'phoenix', 'phx', 'chihuahua', 'cuu',
+                 'queretaro', 'qro', 'guanajuato', 'bjx', 'puebla', 'pbc', 'philadelphia', 'phl',
+                 'san luis potosí', 'slp', 'charlotte', 'clt', 'toronto', 'yyz', 'houston', 'iah', 'vancouver',
+                 'yvr', 'parís', 'cdg', 'ciudad juaréz', 'cjs', 'zacatecas', 'zcl', 'ámsterdam', 'ams',
+                 'atlanta', 'atl', 'ciudad obregon', 'cen', 'madrid', 'mad', 'santiago', 'scl']
+
+def mejor_match(ciudadd):
+    ciudadd.lower()
+    closest, ratio = process.extractOne(ciudadd, ciudades_real)
+    index = ciudades_real.index(closest)
+    return ciudades_real[index+1]
+
