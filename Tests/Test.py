fa667c317f3154a4ff95fabb5466cb6bb68c0e54
itzelfloresc <112104652+itzelfloresc@users.noreply.github.com>
2023-09-23 15:21:53 -0600

    Update Test.py

--- Tests/Test.py
+++ Tests/Test.py
@@ -8,12 +8,12 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.cliente = app.test_client()
 
-#Revisa que las rutas regresen la pagina correcta
+"""Revisa que las rutas regresen la pagina correcta"""
     def test_ticket(self):
         respuesta = self.cliente.post('/ticket', data={'ticket_id': 'kw9f0kwvZJmsukQy'})
         self.assertEqual(respuesta.status_code, 200)
 
-    # Revisa que las rutas regresen la pagina correcta
+    """Revisa que las rutas regresen la pagina correcta"""
     def test_ciudad(self):
         respuesta = self.cliente.post('/ciudad', data={'ciudad': 'Monterrey'})
         self.assertEqual(respuesta.status_code, 200)
