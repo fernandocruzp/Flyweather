c177df68bb2d2954778337c80bf47ff9838b6db0
fernandocruzp <cruzfernando@ciencias.unam.mx>
2023-09-23 12:37:56 -0600

    Tests

--- Tests/Test.py
+++ Tests/Test.py
@@ -2,20 +2,18 @@ import unittest
 from website import vista
 from flask import Flask
 
+
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self) :
+    def setUp(self):
         self.proy = Flask(__name__)
         self.proy.register_blueprint(vista)
 
-        self.proy.config['TESTING'] =True
+        self.proy.config['TESTING'] = True
         self.cliente = self.proy.test_client()
-
 
     def test_ticket(self):
         respuesta = self.cliente.post('/ticket', data={'ticket_id': 'kw9f0kwvZJmsukQy'})
-        
-
 
 
 if __name__ == '__main__':
