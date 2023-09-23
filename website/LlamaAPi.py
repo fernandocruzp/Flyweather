907d9e6d753ee9552f6d98d36cb24c99f12ea00c
itzelfloresc <112104652+itzelfloresc@users.noreply.github.com>
2023-09-23 15:15:29 -0600

    Update LlamaAPi.py

--- website/LlamaAPi.py
+++ website/LlamaAPi.py
@@ -1,10 +1,10 @@
 import requests
 llave="da11b5bd3b772efa31a4aff7e524e16b"
-#Módulo para realizar llamada de API OPENWEATHERMAP
+"""Módulo para realizar llamada de API OPENWEATHERMAP"""
 def realizaBusqueda(lat,lon):
     respuesta = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={llave}")
     if respuesta.status_code == 200:
-        #Regresamos solamente la parte main de la respuesta del json
+        """Regresamos solamente la parte main de la respuesta del json"""
         return respuesta.json()["main"]
     else:
         return "Error"
