45871ecf9fdab995611ce1252b2c7c3ce3929297
fernandocruzp <cruzfernando@ciencias.unam.mx>
2023-10-08 13:36:02 -0600

    Temperaturas ahora se presentan en celsius

--- website/LlamaAPi.py
+++ website/LlamaAPi.py
@@ -18,7 +18,26 @@ def realizaBusqueda(lat, lon):
         clima = respuesta.json()["main"]
         estadoClima = {"clima": respuesta.json()["weather"][0]["main"]}
         clima.update(estadoClima)
-        return clima
+        return modificaTemp(clima)
     else:
         return "Error en la solicitud de clima"
 
+def modificaTemp(clima):
+    """
+    Modifica las temperaturas a grados celcius.
+    
+    Args:
+        clima (dict): diccionario con los datos del clima.
+
+    Returns:
+        dict: Un diccionario con las temepraturas en celsius.
+    """
+    temp_max=float(clima["temp_max"])
+    temp_min=float(clima["temp_min"])
+    temp=float(clima["temp"])
+    sensacion=float(clima["feels_like"])
+    clima["feels_like"]=round(sensacion-273.15,2)
+    clima["temp_max"]=round(temp_max-273.15,2)
+    clima["temp_min"]=round(temp_min-273.15,2)
+    clima["temp"]=round(temp-273.15,2)
+    return clima
